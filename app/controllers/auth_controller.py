9cd775ee505f61dfcd6a069b363d9aff65d67a25
moisesRubens <moisesrubens.pro@gmail.com>
2026-02-07 20:47:33 -0300

    refactor: auth store flow

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,9 +1,9 @@
 
-from services.sale_point_service import get_all_sales_points, make_login
-from models.model import SalePoints
+from services.sale_point_service import get_all_sales_points, make_login, create_sale_point
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from dependecies import validate_token
+from schemas.schema import SalePointResponseDTO
 
 async def login_user(form_data: OAuth2PasswordRequestForm, session):
     return await make_login(form_data, session)
@@ -14,3 +14,7 @@ async def get_all(session, status = Depends(validate_token)):
     
     sales_points = await get_all_sales_points(session)
     return sales_points
+
+async def create(name, email, password, session):
+    sale_point = await create_sale_point(name, email, password, session)
+    return sale_point
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,17 +1,17 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 from dependecies import make_session
-from services.sale_point_service import create_sale_point, get_sale_point
+from services.sale_point_service import get_sale_point
 from models.model import SalePoints
-from controllers.auth_controller import get_all, login_user
+from controllers.auth_controller import get_all, login_user, create
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @auth_router.post("/cadastrar")
 async def store(name: str, email:str, password: str, session = Depends(make_session)):
-    sale_point = await create_sale_point(name, email, password, session)
+    sale_point = await create(name, email, password, session)
     return {"sale_point": sale_point}
 
 
