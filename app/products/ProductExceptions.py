e7f6560a0b5f76af8071d99a15f0fb43d933387d
moisesRubens <moisesrubens.pro@gmail.com>
2026-03-09 17:03:59 -0300

    feat: add new exception to Product domain

--- app/products/ProductExceptions.py
+++ app/products/ProductExceptions.py
@@ -5,3 +5,8 @@ class ExistingProductException(Exception):
 class ProductNotFound(Exception):
     def __init__(self, message="Product not found"):
         super().__init__(message)
+        
+class InsuficientProductsAmountException(Exception):
+    message = "Insufucient products amount"
+    def __inti__(self, message=message):
+        super().__init__(message)
